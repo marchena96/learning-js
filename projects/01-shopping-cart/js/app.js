9341f04b2e913b8cbb4d3780a3e95bb3862998c1
DanielMarchenaM <danielmm0596@gmail.com>
2026-01-30 06:31:54 -0600

    Adding function to retrieve data

--- projects/01-shopping-cart/js/app.js
+++ projects/01-shopping-cart/js/app.js
@@ -1,8 +1,10 @@
+// 1. Initialize variables to reference DOM elements.
 const cart = document.querySelector('#carrito');
 const cartContainer = document.querySelector('#lista-carrito tbody');
 const CLEAN_CART_BTN = document.querySelector('#vaciar-carrito');
 const COURSE_CATALOGUE = document.querySelector('#lista-cursos');
 
+//1.Implement Event Delegation to handle click on dynamic list items
 loadEventListeners();
 function loadEventListeners() {
   COURSE_CATALOGUE.addEventListener('click', addToCart);
@@ -10,8 +12,28 @@ function loadEventListeners() {
 
 
 // Functions
+//2. Validation of the clicked button
 function addToCart(e) {
   if (e.target.classList.contains('agregar-carrito')) {
-    // Traversing the DOM is necessary to read the info of each course ...
+    // Retrieve data from the course object ...
+    const selectedCourse = e.target.parentElement.parentElement;
+    retrieveCourseData(selectedCourse);
+
   }
+}
+
+//2.1 Function to retrieve data from each course
+function retrieveCourseData(course) {
+
+  //Object: Course
+  const courseData = {
+    image: course.querySelector('img').src,
+    title: course.querySelector('h4').textContent,
+    price: course.querySelector('.price span').textContent,
+    id: course.querySelector('.agregar-carrito').getAttribute('data-id'),
+    quantity: 1
+  }
+
+  console.table(courseData);
+
 }
